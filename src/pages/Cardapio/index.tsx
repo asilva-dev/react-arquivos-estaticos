459e1af999ab50c3a1978b9464aff961dfff9d78
asilva-dev <amanda.adgti@gmail.com>
2023-10-13 20:04:23 -0300

    criando os itens

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -4,6 +4,7 @@ import Buscador from "./Buscador";
 import { useState } from "react";
 import Filtros from "./Filtros";
 import Ordenador from "./Ordenador";
+import Itens from "./Itens";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
@@ -23,9 +24,11 @@ export default function Cardapio() {
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
-        <div className={styles.cardapio__filtros}></div>
+        <div className={styles.cardapio__filtros}>
         <Filtros filtro={filtro} setFiltro={setFiltro}/>
         <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+        </div>
+        <Itens busca={busca} filtro={filtro} ordenador={ordenador}/>
       </section>
     </main>
   )
