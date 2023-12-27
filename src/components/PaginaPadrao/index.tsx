5eb64a2057ad77fb1c9278039b2ae8b52e688908
asilvaolabi <asilva@olabi.org.br>
2023-12-27 12:25:47 -0300

    Inserting the router and navigating between pages

--- src/components/PaginaPadrao/index.tsx
+++ src/components/PaginaPadrao/index.tsx
@@ -1,6 +1,8 @@
 
 import styles from './PaginaPadrao.module.scss';
 import {Outlet} from 'react-router-dom';
+import stylesTema from 'styles/Tema.module.scss';
+
 
 export default function PaginaPadrao() {
     
@@ -11,7 +13,7 @@ export default function PaginaPadrao() {
                     A casa do código e da massa
                 </div>
             </header>
-            <div>
+            <div className={stylesTema.container}>
                 <Outlet/>
             </div>
         </>
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -4,6 +4,7 @@ import Filtros from './Filtros';
 import Ordenador from './Ordenador';
 import Itens from './Itens';
 import styles from './Cardapio.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
 
 export default function Cardapio() {
     const [busca, setBusca] = useState('');
@@ -12,7 +13,7 @@ export default function Cardapio() {
     const [ordenador, setOrdenador] = useState('');
     return (
         <section className={styles.cardapio}>
-            <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+            <h3 className={stylesTema.cardapio__titulo}>Cardápio</h3>
             <Buscador busca={busca} setBusca={setBusca} />
             <div className={styles.cardapio__filtros}>
                 <Filtros filtro={filtro} setFiltro={setFiltro}/>
